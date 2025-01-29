8fda04b01d700e016b3a6d2a8c7b5dc84f370d2d
myanoz <myanoz909@gmail.com>
2025-01-29 20:07:09 +0900

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import NewProject from "./components/NewProject";
-import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from 'react';
+import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
+import ProjectsSidebar from './components/ProjectsSidebar';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: null,
+    }));
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
