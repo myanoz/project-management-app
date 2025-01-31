f8cedcec00ccaabe08c3ca11b9fff86f1e5ee1db
myanoz <myanoz909@gmail.com>
2025-01-31 19:55:32 +0900

    0131

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export default function NewTask() {
+  const [enteredTask, setEnteredTask] = useState();
+
+  return (
+    <div className="flex items-center gap-4">
+      <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+      <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+    </div>
+  );
+}
